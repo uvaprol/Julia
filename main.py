8532360040a4776f44b47ab3091a4f3af91b5aae
uvaprol <120366489+uvaprol@users.noreply.github.com>
2024-09-17 14:54:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,38 +3,38 @@ import speech_recognition as sr
 import mouse
 import keyboard
 
-r = sr.Recognizer()
-
-with sr.Microphone() as source:
-
-    print('say')
-
-    r.pause_threshold = 0.8
-
-    r.adjust_for_ambient_noise(source, duration=1)
-
-    audio = r.listen(source)
-
-text = r.recognize_google(audio, language='ru-RU').lower()
-
-print('you say:', text)
-# text = text.split(' ')
-# text = str(text)
-if text.find('включи') != -1:
+def Open_or_close(text, mod):
     text = text.split(' ')
-    print(text[text.index("включи") + 1])
-    open(f'{text[text.index("включи") + 1]}')
-
-
-elif text.find('пиши') != -1:
-    print(text.find('пиши'))
-    message = text[text.find('пиши')+5:].capitalize()
-    print(message)
+    if mod:
+        open(f'{text[text.index("включи") + 1]}')
+    else:
+        close(f'{text[text.index("закрой") + 1]}')
     pass
+def Write_text(text):
+    message = text[text.find('пиши') + 5:].capitalize()
+    return
 
-elif text.find('закрой') != -1:
-    text = text.split(' ')
-    print(text[text.index("закрой") + 1])
-    close(f'{text[text.index("закрой") + 1]}')
-    # keyboard.press('Alt+F4')
-# print(type(str(text)))
+def Julia_brain(text):
+    if text.find('юля') != -1 or text.find('julia'):
+        if text.find('включи') != -1:
+            Open_or_close(text, True)
+        elif text.find('закрой') != -1:
+            Open_or_close(text, False)
+
+        elif text.find('пиши') != -1:
+            Write_text(text)
+
+def main():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('say')
+        r.pause_threshold = 0.8
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+    text = r.recognize_google(audio, language='ru-RU').lower()
+    print('you say:', text)
+    Julia_brain(text)
+
+if __name__ == 'main':
+    main()
+
