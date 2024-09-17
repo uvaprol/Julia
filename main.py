1e3059663b46db39e4d6b34d944e58365d04d9ef
uvaprol <120366489+uvaprol@users.noreply.github.com>
2024-09-17 11:29:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from AppOpener import open
+from AppOpener import open, close
 import speech_recognition as sr
+import mouse
+import keyboard
 
 r = sr.Recognizer()
 
@@ -16,8 +18,23 @@ with sr.Microphone() as source:
 text = r.recognize_google(audio, language='ru-RU').lower()
 
 print('you say:', text)
-text = text.split(' ')
-if text.index('включи') != -1:
+# text = text.split(' ')
+# text = str(text)
+if text.find('включи') != -1:
+    text = text.split(' ')
     print(text[text.index("включи") + 1])
     open(f'{text[text.index("включи") + 1]}')
 
+
+elif text.find('пиши') != -1:
+    print(text.find('пиши'))
+    message = text[text.find('пиши')+5:].capitalize()
+    print(message)
+    pass
+
+elif text.find('закрой') != -1:
+    text = text.split(' ')
+    print(text[text.index("закрой") + 1])
+    close(f'{text[text.index("закрой") + 1]}')
+    # keyboard.press('Alt+F4')
+# print(type(str(text)))
